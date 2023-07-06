fcf6fd4b877157f0297989c105de996a67094c4d
belindaHBTN <6372@holbertonstudents.com>
2023-07-06 01:53:30 -0700

    added save and new methods on storage global variable

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 """This model defines all attributes/methods for other classes"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -21,6 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """return a string to represent the class"""
@@ -29,6 +31,7 @@ class BaseModel:
     def save(self):
         """update the datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """generate a dictionary representation of an instance"""
