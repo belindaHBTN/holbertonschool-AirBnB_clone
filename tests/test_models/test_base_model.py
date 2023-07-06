af1a4db8de0fc25a08d7731530f848cd3698d58d
Kathryn6368 <6368@holbertonstudents.com>
2023-07-05 21:12:10 -0700

    add file - have save, to_dict and id tests working locally

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""Module for testing BaseModel class"""
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """Class for testing the BaseModel"""
+
+    def setUp(self):
+        """Create an instance for use in testing"""
+        self.my_model = BaseModel()
+
+    def test_save(self):
+        """Test is the save method is functioning correctly"""
+        self.my_model.name = "My First Model"
+        self.my_model.my_number = 89
+        update_time = self.my_model.updated_at
+        self.my_model.save()
+        new_update_time = self.my_model.updated_at
+        self.assertNotEqual(update_time, new_update_time)
+
+    def test_to_dict(self):
+        """Test the to_dict method is functioning correctly"""
+        self.assertEqual(type(self.my_model.to_dict()), dict)
+
+    def test_id(self):
+        """Tests the random id alloction"""
+        self.assertTrue(self.my_model.id)
+        self.my_model_2 = BaseModel()
+        self.assertNotEqual(self.my_model.id, self.my_model_2.id)
+
+    def tearDown(self):
+        """Clean up the instance that was used for testing"""
+        pass
