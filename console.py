0990c6ac87dfad022e2e0901843f4fbb1d925772
Kathryn6368 <6368@holbertonstudents.com>
2023-07-08 15:01:39 -0700

    update list and imports with functionality for state, city, amenity, place and review

--- console.py
+++ console.py
@@ -3,15 +3,27 @@
 """A program that contains the entry point of the command interpreter"""
 import cmd
 from models.base_model import BaseModel
-from models.user import User
 from models.engine.file_storage import FileStorage
 from models import storage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """Contains the entry point of the command interpreter"""
-    class_name_dict  = {"BaseModel": BaseModel, "User": User}
-
+    class_name_dict  = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review
+    }
     prompt = "(hbnb)"
 
     def do_quit(self, args):
